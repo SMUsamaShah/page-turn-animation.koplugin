1f57e6d6a0167138513004b7ddfa8f916c1537e2
Muhammad Usama <lifeh2o@gmail.com>
2026-09-12 22:13:01 +0100

    Rename internal plugin identity to Page Turn Animation

--- page-turn-animation.koplugin/main.lua
+++ page-turn-animation.koplugin/main.lua
@@ -4,19 +4,19 @@ local WidgetContainer = require("ui/widget/container/widgetcontainer")
 
 local plugin_dir = debug.getinfo(1, "S").source:match("^@(.*/)") or "./"
 
-local AnimationLab = WidgetContainer:extend{
-    name = "animationlab",
+local PageTurnAnimation = WidgetContainer:extend{
+    name = "pageturnanimation",
     is_doc_only = true,
 }
 
-function AnimationLab:init()
+function PageTurnAnimation:init()
     self.ui.menu:registerToMainMenu(self)
 end
 
-function AnimationLab:turnPageForTest(direction)
+function PageTurnAnimation:turnPageForTest(direction)
     UIManager:nextTick(function()
         if self.ui then
-            self._animationlab_force_once = true
+            self._pageturnanimation_force_once = true
             self.ui:handleEvent(Event:new("GotoViewRel", direction))
         end
     end)
@@ -32,8 +32,8 @@ local function radioItem(text, checked, callback)
 end
 
 local StripReveal = dofile(plugin_dir .. "stripreveal.lua")
-dofile(plugin_dir .. "stripmenu.lua").augment(AnimationLab, radioItem)
-dofile(plugin_dir .. "pageturnhook.lua").augment(AnimationLab, StripReveal)
+dofile(plugin_dir .. "stripmenu.lua").augment(PageTurnAnimation, radioItem)
+dofile(plugin_dir .. "pageturnhook.lua").augment(PageTurnAnimation, StripReveal)
 
 local updater = dofile(plugin_dir .. "pluginupdater.lua").new{
     repository = "SMUsamaShah/page-turn-animation.koplugin",
@@ -41,11 +41,11 @@ local updater = dofile(plugin_dir .. "pluginupdater.lua").new{
     folder = "page-turn-animation.koplugin",
 }
 
-local old_add = AnimationLab.addToMainMenu
-function AnimationLab:addToMainMenu(menu_items)
+local old_add = PageTurnAnimation.addToMainMenu
+function PageTurnAnimation:addToMainMenu(menu_items)
     old_add(self, menu_items)
-    local items = menu_items.animationlab and menu_items.animationlab.sub_item_table
+    local items = menu_items.pageturnanimation and menu_items.pageturnanimation.sub_item_table
     if items then items[#items + 1] = updater:menuItem() end
 end
 
-return AnimationLab
+return PageTurnAnimation
