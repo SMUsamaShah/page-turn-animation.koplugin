30ace08f81fb0afb6a311f4ec267f89487181529
Muhammad Usama <lifeh2o@gmail.com>
2026-09-14 09:43:59 +0100

    Add exact traced page flip preset

--- page-turn-animation.koplugin/stripmenu.lua
+++ page-turn-animation.koplugin/stripmenu.lua
@@ -13,7 +13,8 @@ function Menu.augment(PageTurnAnimation, radioItem)
                 and self.strip_shape ~= "bottom_curve"
                 and self.strip_shape ~= "bottom_curve2"
                 and self.strip_shape ~= "bottom_curve3"
-                and self.strip_shape ~= "page_flip" then
+                and self.strip_shape ~= "page_flip"
+                and self.strip_shape ~= "page_flip_exact" then
             self.strip_shape = "straight"
         end
         self.page_scheduler = G_reader_settings:readSetting("pageturnanimation_page_scheduler") or "free"
@@ -94,6 +95,7 @@ function Menu.augment(PageTurnAnimation, radioItem)
                     settingRadio(self, _("Curved bottom flip 2"), "strip_shape", "bottom_curve2"),
                     settingRadio(self, _("Curved bottom flip 3"), "strip_shape", "bottom_curve3"),
                     settingRadio(self, _("GIF page flip (experimental)"), "strip_shape", "page_flip"),
+                    settingRadio(self, _("Exact traced page flip (480 ms)"), "strip_shape", "page_flip_exact"),
                 },
             },
             {
